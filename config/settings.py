839520b116a1e2c2640964308d1b27e93c39ed01
Alireza <ali1985reza@gmail.com>
2023-01-16 14:38:09 +1100

    preparing for deploy

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com', ]
 
 
 # Application definition
