4ab5246cfd5b142c79d1a6945c7da46a83957b7b
Alireza <ali1985reza@gmail.com>
2023-01-23 23:25:28 +1100

    env

--- config/settings.py
+++ config/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('Secret_key')
+SECRET_KEY = env('Secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com', ]
 
